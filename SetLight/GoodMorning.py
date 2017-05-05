6f22d4837b33c8a2818fe7bd06c38747481ad05d
TimLeaf <timleaf.lei@gmail.com>
2017-05-05 17:48:28 +0900

    Make controller is better.

--- SetLight/GoodMorning.py
+++ SetLight/GoodMorning.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python
 
-import time, os
+import time
+import os
+
 
 def GoodMorning():
     # open light
     os.system("irsend SEND_ONCE light.lircd.conf KEY_OPEN")
 
-    for i in range(1,100):
-        # send color temperature up
-        os.system("irsend SEND_ONCE light.lircd.conf KEY_UP")
-        time.sleep(1)
+    for i in range(1, 10):
+        if i % 2 == 0:
+            # send color temperature up
+            os.system("irsend SEND_START light.lircd.conf KEY_UP; sleepenh 0.3 > 0")
+            os.system("irsend SEND_STOP light.lircd.conf KEY_UP")
 
         # send brightness up
-        os.system("irsend SEND_ONCE light.lircd.conf KEY_BRIGHTNESSUP")
-        time.sleep(1)
-        os.system("irsend SEND_ONCE light.lircd.conf KEY_BRIGHTNESSUP")
-        time.sleep(1)
+        os.system("irsend SEND_START light.lircd.conf KEY_BRIGHTNESSUP; sleepenh 0.3 > 0")
+        os.system("irsend SEND_STOP light.lircd.conf KEY_BRIGHTNESSUP")
+
+        time.sleep(90)
 
 GoodMorning()
 
--- SetLight/GoodNight.py
+++ SetLight/GoodNight.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python
 
+import time
 import os
+
 
 def GoodNight():
     # send color temperature down
     os.system("irsend SEND_START light.lircd.conf KEY_DOWN; sleep 3")
+    time.sleep(1)
     os.system("irsend SEND_STOP light.lircd.conf KEY_DOWN")
 
     # send brightness down
     os.system("irsend SEND_START light.lircd.conf KEY_BRIGHTNESSDOWN; sleep 3")
+    time.sleep(1)
     os.system("irsend SEND_STOP light.lircd.conf KEY_BRIGHTNESSDOWN")
 
     # send the night mode
