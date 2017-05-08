2173d2f35144c705b44f4ee7afe3d45ef30e3cf1
TimLeaf <timleaf.lei@gmail.com>
2017-05-08 23:01:28 +0900

    Use subprocess instead os.system

--- SetLight/GoodMorning.py
+++ SetLight/GoodMorning.py
@@ -1,22 +1,33 @@
 #!/usr/bin/python
 
 import time
-import os
+import subprocess
+
+
+def subprocess_cmd(command):
+    devnull = open('/dev/null', 'w')
+    process = subprocess.Popen(command, stdout=devnull, stderr=devnull, shell=True)
+    process.communicate()[0]
 
 
 def GoodMorning():
     # open light
-    os.system("irsend SEND_ONCE light.lircd.conf KEY_OPEN")
+    # os.system("irsend SEND_ONCE light.lircd.conf KEY_OPEN")
+    subprocess_cmd("irsend SEND_ONCE light.lircd.conf KEY_OPEN")
 
     for i in range(1, 10):
         if i % 2 == 0:
             # send color temperature up
-            os.system("irsend SEND_START light.lircd.conf KEY_UP; sleepenh 0.3 > 0")
-            os.system("irsend SEND_STOP light.lircd.conf KEY_UP")
+            # os.system("irsend SEND_START light.lircd.conf KEY_UP; sleepenh 0.3 > 0")
+            subprocess_cmd("irsend SEND_START light.lircd.conf KEY_UP; sleepenh 0.3 > 0")
+            # os.system("irsend SEND_STOP light.lircd.conf KEY_UP")
+            subprocess_cmd("irsend SEND_STOP light.lircd.conf KEY_UP")
 
         # send brightness up
-        os.system("irsend SEND_START light.lircd.conf KEY_BRIGHTNESSUP; sleepenh 0.3 > 0")
-        os.system("irsend SEND_STOP light.lircd.conf KEY_BRIGHTNESSUP")
+        # os.system("irsend SEND_START light.lircd.conf KEY_BRIGHTNESSUP; sleepenh 0.3 > 0")
+        subprocess_cmd("irsend SEND_START light.lircd.conf KEY_BRIGHTNESSUP; sleepenh 0.3 > 0")
+        # os.system("irsend SEND_STOP light.lircd.conf KEY_BRIGHTNESSUP")
+        subprocess_cmd("irsend SEND_STOP light.lircd.conf KEY_BRIGHTNESSUP")
 
         time.sleep(90)
 
--- SetLight/GoodNight.py
+++ SetLight/GoodNight.py
@@ -1,22 +1,29 @@
 #!/usr/bin/python
 
-import time
-import os
+import subprocess
+
+
+def subprocess_cmd(command):
+    devnull = open('/dev/null', 'w')
+    process = subprocess.Popen(command, stdout=devnull, stderr=devnull, shell=True)
+    process.communicate()[0]
 
 
 def GoodNight():
     # send color temperature down
-    os.system("irsend SEND_START light.lircd.conf KEY_DOWN; sleep 3")
-    time.sleep(1)
-    os.system("irsend SEND_STOP light.lircd.conf KEY_DOWN")
+    # os.system("irsend SEND_START light.lircd.conf KEY_DOWN; sleep 3")
+    subprocess_cmd("irsend SEND_START light.lircd.conf KEY_DOWN; sleep 3")
+    # os.system("irsend SEND_STOP light.lircd.conf KEY_DOWN")
+    subprocess_cmd("irsend SEND_STOP light.lircd.conf KEY_DOWN")
 
     # send brightness down
-    os.system("irsend SEND_START light.lircd.conf KEY_BRIGHTNESSDOWN; sleep 3")
-    time.sleep(1)
-    os.system("irsend SEND_STOP light.lircd.conf KEY_BRIGHTNESSDOWN")
+    # os.system("irsend SEND_START light.lircd.conf KEY_BRIGHTNESSDOWN; sleep 3")
+    subprocess_cmd("irsend SEND_START light.lircd.conf KEY_BRIGHTNESSDOWN; sleep 3")
+    # os.system("irsend SEND_STOP light.lircd.conf KEY_BRIGHTNESSDOWN")
+    subprocess_cmd("irsend SEND_STOP light.lircd.conf KEY_BRIGHTNESSDOWN")
 
     # send the night mode
-    os.system("irsend SEND_ONCE light.lircd.conf KEY_N")
+    # os.system("irsend SEND_ONCE light.lircd.conf KEY_N")
+    subprocess_cmd("irsend SEND_ONCE light.lircd.conf KEY_N")
 
 GoodNight()
-
